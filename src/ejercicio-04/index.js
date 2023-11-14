c843dd28906168b78f8106e1859e2a7e3135f3c0
XaviBarragan <xbarragan73@gmail.com>
2023-11-14 16:18:39 +0100

    Añadir ejercicio-04

--- src/ejercicio-04/index.js
+++ src/ejercicio-04/index.js
@@ -1,1 +1,17 @@
-// Escribe tu código aquí:
+// Ejercicio 04: Expresiones en Template Literals
+// Dado un arreglo de números, utiliza template literals para mostrar si cada número es par o impar.
+
+// Defino el Array numeros
+const numeros = [2, 7, 4, 9, 6]
+
+// Utilizo una ArrowFunction y template literals para decir si cada numero es par o impar
+numeros.forEach((numero) => {
+    console.log(`El número ${numero} es ${numero % 2 === 0? 'par' : 'impar'}.`)
+})
+
+// Resultado:
+// El número 2 es par.
+// El número 7 es impar.
+// El número 4 es par.
+// El número 9 es impar.
+// El número 6 es par.
