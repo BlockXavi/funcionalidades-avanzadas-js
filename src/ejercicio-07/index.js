a031f7b5caea4bd9182bc1be4621fa89c5492864
XaviBarragan <xbarragan73@gmail.com>
2023-11-14 16:43:54 +0100

    Añadir ejercicio-07

--- src/ejercicio-07/index.js
+++ src/ejercicio-07/index.js
@@ -1,1 +1,17 @@
-// Escribe tu código aquí:
+// Ejercicio 07: Usando Spread Operator
+// Dados dos arreglos, utiliza el spread operator para combinarlos en uno solo.
+
+// Defino los dos arrays a unir
+const array1 = [1, 2, 3]
+const array2 = [4, 5, 6]
+
+// Aplico el Spread Operator para unirlos
+const arrayUnion = [...array1, ...array2]
+const arrayUnionReves = [...array2, ...array1]
+
+// Los muestro
+console.log(arrayUnion)
+console.log(arrayUnionReves)
+// Resultado:
+// [1, 2, 3, 4, 5, 6]
+// [4, 5, 6, 1, 2, 3]
